0a3d1889fe5aefb88704e7365888762002dc711b
jithinmca <myjithu@gmail.com>
2023-01-09 13:22:19 +0530

    Initial commit

---
+++ base/app/application-user/application-user/application-user.base.model.ts
@@ -0,0 +1,11 @@
+export interface ApplicationUserBase {
+	createdBy: string;
+	createdDate: Date;
+	modifiedDate: Date;
+	sid: string;
+	modifiedBy: string;
+	lastName: string;
+	email: string;
+	userRoles: string;
+	firstName: string;
+}
---
+++ base/app/countries/countries/countries.base.model.ts
@@ -0,0 +1,10 @@
+export interface CountriesBase {
+	modifiedBy: string;
+	countryName: string;
+	createdBy: string;
+	modifiedDate: Date;
+	currency: string;
+	countryCode: string;
+	sid: string;
+	createdDate: Date;
+}
